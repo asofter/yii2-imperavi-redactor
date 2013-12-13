d6f6621eda17ec23e43cf42718945bcda0c32792
Alex Softer <alwex10@gmail.com>
2013-12-14 00:03:54 +0400

    new assets

---
+++ FullscreenImperaviRedactorPluginAsset.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\imperavi;
+use yii\web\AssetBundle;
+
+/**
+ * @author Alexander Yaremchuk <alwex10@gmail.com>
+ * @since 1.0
+ */
+class FullscreenImperaviRedactorPluginAsset extends AssetBundle
+{
+    public $sourcePath = '@yii/imperavi/assets/plugins/fullscreen';
+    public $js = [
+        'fullscreen.js'
+    ];
+    public $css = [
+
+    ];
+    public $depends = [
+        'yii\imperavi\ImperaviRedactorAsset'
+    ];
+}
