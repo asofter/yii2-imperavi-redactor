09689f7dddd55fea8fa08198f4f15bec9231bcef
kayalion <kayalion@kayalion.net>
2015-09-04 12:50:38 +0200

    Update definedlinks.js
    - Preselect the URL option if the set URL is available in the options
    - Added empty value for when the URL input is not available in the options
    - Update selection when URL is edited

--- assets/plugins/definedlinks/definedlinks.js
+++ assets/plugins/definedlinks/definedlinks.js
@@ -13,8 +13,15 @@
 			load: function()
 			{
 				var $select = $('<select id="redactor-defined-links" />');
+				$select.append($('<option value="">---</option>'));
+
+				var $input = $('#redactor-link-url');
+
 				$('#redactor-modal-link-insert').prepend($select);
 
+				this.definedlinks.formOptions = $select;
+				this.definedlinks.formUrl = $input;
+				this.definedlinks.formName = $('#redactor-link-url-text');
 				this.definedlinks.storage = {};
 
 				$.getJSON(this.opts.definedLinks, $.proxy(function(data)
@@ -26,25 +33,42 @@
 
 					}, this));
 
+					$input.on('keyup', $.proxy(this.definedlinks.change, this)).trigger('keyup');
 					$select.on('change', $.proxy(this.definedlinks.select, this));
-
 				}, this));
-
 			},
 			select: function(e)
 			{
 				var key = $(e.target).val();
 				var name = '', url = '';
+
 				if (key !== 0)
 				{
 					name = this.definedlinks.storage[key].name;
 					url = this.definedlinks.storage[key].url;
 				}
 
-				$('#redactor-link-url').val(url);
+				this.definedlinks.formUrl.val(url);
 
 				var $el = $('#redactor-link-url-text');
-				if ($el.val() === '') $el.val(name);
+				if (this.definedlinks.formName.val() === '') this.definedlinks.formName.val(name);
+			},
+			change: function(e)
+			{
+				var url = $(e.target).val();
+				var found = false;
+
+				for (key in this.definedlinks.storage)
+				{
+					if (this.definedlinks.storage[key].url == url)
+					{
+						this.definedlinks.formOptions.val(key);
+
+						return;
+					}
+				}
+
+				if (found === false) this.definedlinks.formOptions.val('');
 			}
 		};
 	};
